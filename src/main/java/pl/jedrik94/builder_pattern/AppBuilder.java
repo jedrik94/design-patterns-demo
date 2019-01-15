62cb8272e841b43eb325182c42c8416f764cc059
Wojtkowiak Jedrzej <jedrik94@gmail.com>
2019-01-15 10:54:49 +0100

    Cannot use worksAs() as fluent builder.

--- src/main/java/pl/jedrik94/builder_pattern/AppBuilder.java
+++ src/main/java/pl/jedrik94/builder_pattern/AppBuilder.java
@@ -1,14 +1,15 @@
 package pl.jedrik94.builder_pattern;
 
+import pl.jedrik94.builder_pattern.model.EmployeeBuilder;
 import pl.jedrik94.builder_pattern.model.Person;
-import pl.jedrik94.builder_pattern.model.PersonBuilder;
 
 public class AppBuilder {
     public static void main(String[] args) {
-        PersonBuilder personBuilder = new PersonBuilder();
+        EmployeeBuilder personBuilder = new EmployeeBuilder();
 
         Person jedrzej = personBuilder
                 .withName("Jedrzej")
+//              .worksAs("software engineer")
                 .build();
 
         System.out.println(jedrzej);
