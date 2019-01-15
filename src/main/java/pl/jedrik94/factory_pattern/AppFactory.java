f1daeeb6625a991130df99bb8e71869383f89277
Wojtkowiak Jedrzej <jedrik94@gmail.com>
2019-01-15 13:51:23 +0100

    Basic Method Factory.

--- src/main/java/pl/jedrik94/factory_pattern/AppFactory.java
+++ src/main/java/pl/jedrik94/factory_pattern/AppFactory.java
@@ -1,4 +1,13 @@
 package pl.jedrik94.factory_pattern;
 
+import pl.jedrik94.factory_pattern.model.Point;
+
 public class AppFactory {
+    public static void main(String[] args) {
+        Point dot1 = Point.newCartesianPoint(1, 0 );
+        Point dot2 = Point.newPolarPoint(1, 0);
+
+        System.out.println(dot1);
+        System.out.println(dot2);
+    }
 }
--- src/main/java/pl/jedrik94/factory_pattern/model/Point.java
+++ src/main/java/pl/jedrik94/factory_pattern/model/Point.java
@@ -3,16 +3,24 @@ package pl.jedrik94.factory_pattern.model;
 public class Point {
     private double x, y;
 
-    public Point(double a, double b, CoordinateSystem coordinateSystem) {
-        switch (coordinateSystem) {
-            case CARTESIAN:
-                x = a;
-                y = b;
-                break;
-            case POLAR:
-                x = a * Math.cos(b);
-                y = a * Math.sin(a);
-                break;
-        }
+    private Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Point newCartesianPoint(double x, double y) {
+        return new Point(x, y);
+    }
+
+    public static Point newPolarPoint(double rho, double theta) {
+        return new Point(rho * Math.cos(theta), rho * Math.sin(theta));
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
     }
 }
